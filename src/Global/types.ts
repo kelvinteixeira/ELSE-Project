790a6770804c00acf04342f6a202c349c2e7a253
kesantos <kesantos@paschoalotto.com.br>
2023-06-07 01:15:43 -0300

    feat(Types): Created global types file

---
+++ src/Global/types.ts
@@ -0,0 +1,13 @@
+export type CarProps = {
+  image?: string;
+  model?: string;
+  price?: number;
+  brand?: string;
+  year?: number;
+  color?: string;
+  city?: string;
+  plate?: string;
+  mileage?: number;
+  registerDate?: string;
+  visualizationCounter?: number;
+};
