fee14f305c0e7eac501cc62ec9a61624c172d5cf
kesantos <kesantos@paschoalotto.com.br>
2023-06-06 22:48:48 -0300

    feat(Mock): Created mock

---
+++ src/mock.ts
@@ -0,0 +1,62 @@
+export const fakeCars = [
+  {
+    id: 1,
+    image:
+      "https://media.autoexpress.co.uk/image/private/s--X-WVjvBW--/f_auto,t_content-image-full-desktop@1/v1617294044/autoexpress/2021/04/Ferrari%20812%20GTS-2.jpg",
+    model: "812 GTS",
+    price: 1000000,
+    brand: "Ferrari",
+    year: 1999,
+    visualizationCounter: 823,
+  },
+  {
+    id: 2,
+    image:
+      "https://media.autoexpress.co.uk/image/private/s--X-WVjvBW--/f_auto,t_content-image-full-desktop@1/v1617294044/autoexpress/2021/04/Ferrari%20812%20GTS-2.jpg",
+    model: "812 GTS",
+    price: 1000000,
+    brand: "Ferrari",
+    year: 1999,
+    visualizationCounter: 1823,
+  },
+  {
+    id: 3,
+    image:
+      "https://media.autoexpress.co.uk/image/private/s--X-WVjvBW--/f_auto,t_content-image-full-desktop@1/v1617294044/autoexpress/2021/04/Ferrari%20812%20GTS-2.jpg",
+    model: "812 GTS",
+    price: 1000000,
+    brand: "Ferrari",
+    year: 1999,
+    visualizationCounter: 323,
+  },
+  {
+    id: 4,
+    image:
+      "https://media.autoexpress.co.uk/image/private/s--X-WVjvBW--/f_auto,t_content-image-full-desktop@1/v1617294044/autoexpress/2021/04/Ferrari%20812%20GTS-2.jpg",
+    model: "812 GTS",
+    price: 1000000,
+    brand: "Ferrari",
+    year: 1999,
+    visualizationCounter: 323,
+  },
+  {
+    id: 5,
+    image:
+      "https://media.autoexpress.co.uk/image/private/s--X-WVjvBW--/f_auto,t_content-image-full-desktop@1/v1617294044/autoexpress/2021/04/Ferrari%20812%20GTS-2.jpg",
+    model: "812 GTS",
+    price: 1000000,
+    brand: "Ferrari",
+    year: 1999,
+    visualizationCounter: 323,
+  },
+  {
+    id: 6,
+    image:
+      "https://media.autoexpress.co.uk/image/private/s--X-WVjvBW--/f_auto,t_content-image-full-desktop@1/v1617294044/autoexpress/2021/04/Ferrari%20812%20GTS-2.jpg",
+    model: "812 GTS",
+    price: 1000000,
+    brand: "Ferrari",
+    year: 1999,
+    visualizationCounter: 323,
+  },
+];
